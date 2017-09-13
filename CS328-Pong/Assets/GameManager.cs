e9d98ccb36a34211af722ef9e96832998069f825
brmarino <brandonrmarino@gmail.com>
2017-09-13 15:07:52 -0700

    Finished Pong

--- CS328-Pong/Assets/GameManager.cs
+++ CS328-Pong/Assets/GameManager.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public static int Player1Score = 0;
+    public static int Player2Score = 0;
+
+    // GUISkin?
+    public GUISkin layout;
+
+    GameObject theBall;
+
+    // Use this for initialization
+    void Start () {
+
+        theBall = GameObject.FindGameObjectWithTag("Ball");
 		
 	}
 	
@@ -13,4 +23,43 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public static void Score (string wallID)
+    {
+        if (wallID == "RightWall")
+        {
+            Player1Score++;
+        }
+        else
+        {
+            Player2Score++;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 50, 20, 100, 100), "" + Player1Score);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + Player2Score);
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53), "RESTART"))
+        {
+            Player1Score = 0;
+            Player2Score = 0;
+            theBall.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+        }
+
+        if (Player1Score == 5)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+            theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+        else if (Player2Score == 5)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+            theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+
+    }
+
 }
