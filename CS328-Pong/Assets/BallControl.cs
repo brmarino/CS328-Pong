cb512b2346c44e25680cb13f3bff1f8654fef8ca
brmarino <brandonrmarino@gmail.com>
2017-09-13 13:36:10 -0700

    The Beginning of Pong

---
+++ CS328-Pong/Assets/BallControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if(rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    void ResetBall()
+    {
+        vel = Vector2.zero;
+        rb2d.velocity = vel;
+        transform.position = Vector2.zero;
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("GoBall", 2);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (collision.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+
+
+}
